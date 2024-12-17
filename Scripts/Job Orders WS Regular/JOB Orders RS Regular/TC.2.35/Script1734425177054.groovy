6f816dae8f9515537753fd25a194c3e624b0366a
Angel Stephanie <angelnapitupulu85@mail,com>
2024-12-17 16:47:55 +0700

    Pengerjaan Test Case pada WS Regular

---
+++ Scripts/Job Orders WS Regular/JOB Orders RS Regular/TC.2.35/Script1734425177054.groovy
@@ -0,0 +1,53 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser(GlobalVariable.baseURL)
+
+WebUI.maximizeWindow(FailureHandling.STOP_ON_FAILURE)
+
+WebUI.setText(findTestObject('Object Repository/Page_ST Engineering Aerospace/input_Log in_Input.Username'), GlobalVariable.username)
+
+WebUI.setText(findTestObject('Object Repository/Page_ST Engineering Aerospace/input_Log in_Input.Password'), GlobalVariable.password)
+
+WebUI.click(findTestObject('Object Repository/Page_ST Engineering Aerospace/button_Log in'))
+
+WebUI.click(findTestObject('Object Repository/Page_Workchestra/div_Job No.  74016580000298Released to Sign_3895e8'))
+
+WebUI.click(findTestObject('Object Repository/Page_Workchestra/div_Job Details Work Stages Defect Rectific_a011ce'))
+
+WebUI.click(findTestObject('Object Repository/Page_Workchestra/a_CRR Forms'))
+
+WebUI.click(findTestObject('Object Repository/Page_Workchestra/div_Work Stages Defect Rectification Record_04ac9e'))
+
+WebUI.click(findTestObject('Object Repository/Page_Workchestra/tab_CRR Form TypeForm NameCrr No.Issue No.S_687c9e'))
+
+WebUI.click(findTestObject('Object Repository/Page_Workchestra/button_Open'))
+
+WebUI.click(findTestObject('Object Repository/Page_Workchestra/div_Related Router No item selected  Form N_127ca4'))
+
+WebUI.click(findTestObject('Object Repository/Page_Workchestra/button_Approve'))
+
+WebUI.click(findTestObject('Object Repository/Page_Workchestra/div_Confirm and Approve CRR Form Please pla_0535c9'))
+
+WebUI.click(findTestObject('Object Repository/Page_Workchestra/img_Created_cursor-pointer'))
+
+WebUI.click(findTestObject('Object Repository/Page_Workchestra/div_Status Completed Completed By EmpNo8030_90be1a'))
+
+WebUI.closeBrowser()
+
